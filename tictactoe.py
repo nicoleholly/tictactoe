7f9616c9975a76ffba09f612eec5c9386103a3b9
Nicole Baram <nhb228@nyu.edu>
2019-02-27 18:50:40 -0500

    clean up & doc strings

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ class TicTacToe():
         self.board = [['-'] * 3 for _ in range(3)]
         self.player_one_moves = True
 
+# -- player methods --
     def get_current_player(self):
         if self.player_one_moves:
             return 1
@@ -11,14 +12,23 @@ class TicTacToe():
     def toggle_current_player(self):
         self.player_one_moves = not self.player_one_moves
 
-    def print_board (self):
-        for row in range(3):
-            for col in range(3):
-                print(self.board[row][col], end='')
-                if col < 2:
-                    print('|', end = '')
-                else:
-                    print(' ')
+# -- token methods --
+    def add_token(self, row, col, token):
+        self.board[row][col] = token
+
+# -- validation methods --
+    def is_valid_move(self, row, col):
+        if self.board[row][col] == '-':
+            return True
+        return False
+
+    def check_for_winning_move(self):
+        if self.player_one_moves:
+            token = 'X'
+        else:
+            token = 'O'
+
+        ### finish..
 
     def check_board_is_full(self):
         for row in range(3):
@@ -27,13 +37,16 @@ class TicTacToe():
                     return False
         return True
 
-    def is_valid_move(self, row, col):
-        if self.board[row][col] == '-':
-            return True
-        return False
 
-    def add_token(self, row, col, token):
-        self.board[row][col] = token
+# -- print methods --
+    def print_board (self):
+        for row in range(3):
+            for col in range(3):
+                print(self.board[row][col], end='')
+                if col < 2:
+                    print('|', end = '')
+                else:
+                    print(' ')
 
     def print_header(self):
         board.print_board()
@@ -47,11 +60,12 @@ def main():
         row, col = int(input('Please type the row [0-2]: ')), int(input('Please type the column [0-2]: '))
         if board.is_valid_move(row, col):
             if board.get_current_player() == 1:
-                board.add_token(row, col, 'X')
-                board.toggle_current_player()
+                token = 'X'
             else:
-                board.add_token(row, col, 'O')
-                board.toggle_current_player()
+                token = 'O'
+            board.add_token(row, col, token)
+            board.toggle_current_player()
+
         else:
             print('Invalid move player {}. Please try again'.format(board.get_current_player()))
 
